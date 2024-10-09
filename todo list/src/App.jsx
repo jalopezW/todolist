2532b2500d6b618c5284abd698e39489a54cf847
Jesus <jlopezloyolamarymount@gmail.com>
2024-10-09 13:07:43 -0700

    Added deletion for todo list

--- todo list/src/App.jsx
+++ todo list/src/App.jsx
@@ -1,35 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import TodoList from './TodoList';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-//   return (
-//     <>
-//       <div>
-//         <a href="https://vitejs.dev" target="_blank">
-//           <img src={viteLogo} className="logo" alt="Vite logo" />
-//         </a>
-//         <a href="https://react.dev" target="_blank">
-//           <img src={reactLogo} className="logo react" alt="React logo" />
-//         </a>
-//       </div>
-//       <h1>Vite + React</h1>
-//       <div className="card">
-//         <button onClick={() => setCount((count) => count + 1)}>
-//           count is {count}
-//         </button>
-//         <p>
-//           Edit <code>src/App.jsx</code> and save to test HMR
-//         </p>
-//       </div>
-//       <p className="read-the-docs">
-//         Click on the Vite and React logos to learn more
-//       </p>
-//     </>
-//   )
+  const [todos, setTodos] = useState([
+    {id: 1, content: 'Meet Masao for Udon'},
+    {id: 2, content: 'Fix up these horrible React notes'},
+  ]);
+  function deleteTodo(id) {
+    setTodos(todos => todos.filter(todo => todo.id !== id));
+  }
+  return (
+    <div>
+      <h1>Things To Do</h1>
+      <TodoList todos={todos} deleter={deleteTodo}/>
+    </div>
+  );
 }
 
-export default App
+export default App;
